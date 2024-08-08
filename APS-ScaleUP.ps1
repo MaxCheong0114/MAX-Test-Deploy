9162bff8f88e8f6d5d9b5675347c01bf7446dba3
MaxCheong0114 <maxcheong0114@gmail.com>
2024-08-09 01:26:17 +0800

    Delete APS-ScaleUP.ps1

--- APS-ScaleUP.ps1
+++
@@ -1,6 +0,0 @@
-﻿$subscription = "Visual Studio Enterprise Subscription – MPN"
-$resourceGroup = "Max_Testing_ASP"
-$appServicePlan = "MAX-ASP-SEA-TestLab-01"
-$sku = "P1" 
-
-az appservice plan update -name $appServicePlan -resource-group $resourceGroup -subscription $subscription -sku $sku

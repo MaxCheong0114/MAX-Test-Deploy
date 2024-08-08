2186d5e4927bad64dbe1c60859a746477acd4e91
MaxCheong0114 <maxcheong0114@gmail.com>
2024-08-09 01:35:46 +0800

    Update ASP-ScaleUP.ps1

--- ASP-ScaleUP.ps1
+++ ASP-ScaleUP.ps1
@@ -1,6 +1,5 @@
-﻿$subscription = "Visual Studio Enterprise Subscription – MPN"
 $resourceGroup = "Max_Testing_ASP"
 $appServicePlan = "MAX-ASP-SEA-TestLab-01"
-$sku = "P1" 
 
-az appservice plan update -name $appServicePlan -resource-group $resourceGroup -subscription $subscription -sku $sku
+az appservice plan update -name $appServicePlan -resource-group $resourceGroup --sku P1V3
+

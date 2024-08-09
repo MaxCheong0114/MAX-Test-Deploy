453e41b97f9968e5b07815c5f56eec01b51fabe2
MaxCheong0114 <maxcheong0114@gmail.com>
2024-08-09 14:12:09 +0800

    Add files via upload

---
+++ ASP-ScaleDown.ps1
@@ -0,0 +1,6 @@
+﻿﻿$subscription = "Visual Studio Enterprise Subscription – MPN"
+$resourceGroup = "Max_Testing_ASP"
+$appServicePlan = "MAX-ASP-SEA-TestLab-01"
+$sku = "S1" 
+
+az appservice plan update -name $appServicePlan -resource-group $resourceGroup -subscription $subscription -sku $sku

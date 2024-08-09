fbc2d404e7ce1321b2390b583d2179a75ee11ddb
MaxCheong0114 <maxcheong0114@gmail.com>
2024-08-09 14:25:10 +0800

    Update ASP-ScaleDown.ps1

--- ASP-ScaleDown.ps1
+++ ASP-ScaleDown.ps1
@@ -1,6 +1,4 @@
-﻿﻿$subscription = "Visual Studio Enterprise Subscription – MPN"
 $resourceGroup = "Max_Testing_ASP"
 $appServicePlan = "MAX-ASP-SEA-TestLab-01"
-$sku = "S1" 
 
-az appservice plan update -name $appServicePlan -resource-group $resourceGroup -subscription $subscription -sku $sku
+az appservice plan update --name MyAppServicePlan --resource-group MyResourceGroup --sku S1

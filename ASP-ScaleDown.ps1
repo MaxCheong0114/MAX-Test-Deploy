23c58fbeb9d68211bbd63e574a04ccc9c9f25484
MaxCheong0114 <maxcheong0114@gmail.com>
2024-08-09 14:37:59 +0800

    Update ASP-ScaleDown.ps1

--- ASP-ScaleDown.ps1
+++ ASP-ScaleDown.ps1
@@ -1,4 +1,4 @@
 $resourceGroup = "Max_Testing_ASP"
 $appServicePlan = "MAX-ASP-SEA-TestLab-01"
 
-az appservice plan update --name $appServicePlan --resource-group $appServicePlan --sku S1
+az appservice plan update --name $appServicePlan --resource-group $resourceGroup --sku S1
